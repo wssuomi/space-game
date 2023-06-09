4623968cb031bbbd25e2f4f48e8625d705850f14
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-06-09 16:46:15 +0300

    added spawning rocks

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,63 @@
 use bevy::{prelude::*, window::PrimaryWindow};
-// use rand::prelude::*;
+use rand::prelude::*;
 
 pub const PLAYER_SPEED: f32 = 480.0;
 pub const PLAYER_SIZE: f32 = 100.0;
+pub const ROCK_COOLDOWN: f32 = 2.0;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.2)))
+        .init_resource::<RockSpawnTimer>()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Space game".into(),
                 resolution: (900.0, 900.0).into(),
+                resize_constraints: WindowResizeConstraints {
+                    max_width: 900.0,
+                    max_height: 900.0,
+                    min_width: 900.0,
+                    min_height: 900.0,
+                },
+                resizable: false,
+
                 ..default()
             }),
             ..default()
         }))
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_system(spawn_rocks_over_time)
+        .add_system(tick_rock_spawn_timer)
         .add_system(player_movement)
         .run();
+}
+
+#[derive(Resource)]
+pub struct RockSpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for RockSpawnTimer {
+    fn default() -> Self {
+        RockSpawnTimer {
+            timer: Timer::from_seconds(ROCK_COOLDOWN, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+enum RockSize {
+    Big,
+    Normal,
+    Small,
+}
+
+#[derive(Component)]
+pub struct Rock {
+    size: RockSize,
 }
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
@@ -28,9 +67,6 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         ..default()
     });
 }
-
-#[derive(Component)]
-pub struct Player {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -101,3 +137,38 @@ pub fn player_movement(
         transform.translation = new_translation;
     }
 }
+
+pub fn spawn_rocks_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    rock_spawn_timer: Res<RockSpawnTimer>,
+) {
+    if rock_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        let mut rng = thread_rng();
+        let (rock_size, rock_sprite) = match rng.gen_range(0..3) {
+            0 => (RockSize::Small, "sprites/small_rock.png"),
+            1 => (RockSize::Normal, "sprites/normal_rock.png"),
+            _ => (RockSize::Big, "sprites/big_rock.png"),
+        };
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load(rock_sprite),
+                ..default()
+            },
+            Rock { size: rock_size },
+        ));
+    }
+}
+
+pub fn tick_rock_spawn_timer(mut rock_spawn_timer: ResMut<RockSpawnTimer>, time: Res<Time>) {
+    rock_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn player_rock_collision() {}
