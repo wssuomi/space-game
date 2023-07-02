87d98c00811b0e7540357c88445585708a1a7400
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-07-02 22:01:24 +0300

    resetting score after exiting game state

--- src/score.rs
+++ src/score.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::state::AppState;
+
 pub const SCORE_COOLDOWN: f32 = 1.0;
 
 #[derive(Resource)]
@@ -26,7 +28,7 @@ impl Default for ScoreTimer {
     }
 }
 
-pub fn add_score_over_timer(mut score: ResMut<Score>, score_timer: Res<ScoreTimer>) {
+pub fn add_score_over_time(mut score: ResMut<Score>, score_timer: Res<ScoreTimer>) {
     if score_timer.timer.finished() {
         score.value += 5;
         println!("Score: {}", score.value);
@@ -37,12 +39,33 @@ pub fn tick_score_timer(mut score_timer: ResMut<ScoreTimer>, time: Res<Time>) {
     score_timer.timer.tick(time.delta());
 }
 
+pub fn add_score_resource(mut commands: Commands) {
+    commands.insert_resource(Score::default())
+}
+
+pub fn add_score_timer_resource(mut commands: Commands) {
+    commands.insert_resource(ScoreTimer::default())
+}
+
+pub fn remove_score_resource(mut commands: Commands) {
+    commands.remove_resource::<Score>();
+}
+
+pub fn remove_score_timer_resource(mut commands: Commands) {
+    commands.remove_resource::<ScoreTimer>();
+}
+
 pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Score>()
-            .init_resource::<ScoreTimer>()
-            .add_systems((add_score_over_timer, tick_score_timer));
+        app.add_systems(
+            (add_score_resource, add_score_timer_resource).in_schedule(OnEnter(AppState::Game)),
+        )
+        .add_systems((add_score_over_time, tick_score_timer).in_set(OnUpdate(AppState::Game)))
+        .add_systems(
+            (remove_score_resource, remove_score_timer_resource)
+                .in_schedule(OnExit(AppState::Game)),
+        );
     }
 }
