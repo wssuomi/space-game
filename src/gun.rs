9c478d1e0d9004661170804a1f898a02fc33d61f
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-07-14 14:33:01 +0300

    added cooldown to shooting

--- src/gun.rs
+++ src/gun.rs
@@ -1,21 +1,36 @@
 use bevy::prelude::*;
 
-use crate::{arena::ARENA_HEIGHT, assets::SpriteAssets, player::Player};
+use crate::{arena::ARENA_HEIGHT, assets::SpriteAssets, player::Player, state::AppState};
 
 pub const BULLET_SPEED: f32 = 100.0;
 pub const BULLET_HEIGHT: f32 = 30.0;
+pub const BULLET_COOLDOWN: f32 = 2.0;
 
 #[derive(Component)]
 pub struct Bullet;
+
+#[derive(Resource)]
+pub struct BulletCooldownTimer {
+    timer: Timer,
+}
+
+impl Default for BulletCooldownTimer {
+    fn default() -> Self {
+        BulletCooldownTimer {
+            timer: Timer::from_seconds(BULLET_COOLDOWN, TimerMode::Once),
+        }
+    }
+}
 
 pub fn shoot(
     mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     handles: Res<SpriteAssets>,
     player_query: Query<&Transform, With<Player>>,
+    mut bullet_cooldown_timer: ResMut<BulletCooldownTimer>,
 ) {
     if let Ok(transform) = player_query.get_single() {
-        if keyboard_input.pressed(KeyCode::Space) {
+        if keyboard_input.pressed(KeyCode::Space) && bullet_cooldown_timer.timer.finished() {
             commands.spawn((
                 SpriteBundle {
                     transform: *transform,
@@ -24,6 +39,7 @@ pub fn shoot(
                 },
                 Bullet {},
             ));
+            bullet_cooldown_timer.timer.reset();
         }
     }
 }
@@ -46,10 +62,35 @@ pub fn despawn_off_screen_bullets(
     }
 }
 
+pub fn add_bullet_cooldown_timer_resource(mut commands: Commands) {
+    commands.insert_resource(BulletCooldownTimer::default())
+}
+
+pub fn remove_bullet_cooldown_timer_resource(mut commands: Commands) {
+    commands.remove_resource::<BulletCooldownTimer>();
+}
+
+pub fn tick_bullet_cooldown_timer(
+    mut bullet_cooldown_timer: ResMut<BulletCooldownTimer>,
+    time: Res<Time>,
+) {
+    bullet_cooldown_timer.timer.tick(time.delta());
+}
+
 pub struct GunPlugin;
 
 impl Plugin for GunPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems((shoot, move_bullets, despawn_off_screen_bullets));
+        app.add_system(add_bullet_cooldown_timer_resource.in_schedule(OnEnter(AppState::Game)))
+            .add_systems(
+                (
+                    shoot,
+                    move_bullets,
+                    despawn_off_screen_bullets,
+                    tick_bullet_cooldown_timer,
+                )
+                    .in_set(OnUpdate(AppState::Game)),
+            )
+            .add_system(remove_bullet_cooldown_timer_resource.in_schedule(OnExit(AppState::Game)));
     }
 }
